d20a82cae0fd3456412c063bd7f0c7f0b10f3dc6
fred benson <beetardation@gmail.com>
2015-12-27 19:49:20 +0000

    fixed syntax error in http server creation

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var http = require("http");
 
-http.createServer(function(request, response) {
+var server = http.createServer(function(request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello World");
-  response.end();
-}).listen(8888);
+  response.end('Hello http');
+});
+server.listen(8080);
